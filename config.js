72ce182a449f0e08abf4a2b9b5e7e3ad4a1cbd1e
FuryyHandsome <87662932+FuryyHandsome@users.noreply.github.com>
2021-07-19 23:13:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Xuân yêu Phát nhiều nhiều nhiều <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tí tối anh qua gặp em nhé công chúa.',
+    messDesc: 'Tí tối anh qua gặp em nhé công chúa <3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
